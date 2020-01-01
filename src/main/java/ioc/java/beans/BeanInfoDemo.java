392c6baf3cb1ee001866e0ce629c34d70a9cb9dd
Mengleifeng <fmleing@163.com>
2020-01-01 12:37:59 +0800

    BeanInfo例子
    通过获取Preson pojo的beans属性信息，进行展示。

---
+++ src/main/java/ioc/java/beans/BeanInfoDemo.java
@@ -0,0 +1,18 @@
+package ioc.java.beans;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.util.stream.Stream;
+
+public class BeanInfoDemo {
+
+    public static void main(String[] args) throws IntrospectionException {
+        // stopClass
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class,Object.class);
+        Stream.of(beanInfo.getPropertyDescriptors())
+                .forEach(propertyDescriptor -> {
+                    System.out.println(propertyDescriptor);
+                });
+    }
+}
